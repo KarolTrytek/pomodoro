4fa4569a996cf3f218054401b3da729f55c29703
cad <cad@pk.edu.pl>
2023-05-15 10:27:13 +0200

    first commit

---
+++ lib/pomodoroscreen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:pomodorro/utils.dart';
+import 'package:pomodorro/widget/timercard.dart';
+
+class PomodoroScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext) {
+    return Scaffold(
+      backgroundColor: Colors.redAccent,
+      appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.redAccent,
+          title: Text(
+            "Pomodoro",
+            style: textStyle(25, Colors.white, FontWeight.w700),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.refresh,
+                color: Colors.white,
+              ),
+              iconSize: 40,
+            )
+          ]),
+      body: SingleChildScrollView(
+        child: Container(
+          alignment: Alignment.center,
+            child: Column(
+              children: [
+                SizedBox(height: 15,),
+                TimerCard(),
+              ],
+            ),
+        ),
+      ),
+    );
+  }
+}
